11490cd19ff71481af74f084fe198ee3be434a31
sigristarisa <arisasigrist.ch@gmail.com>
2022-05-15 16:56:14 +0200

    figuring out to render checkbox items

--- src/components/AnswerItem.js
+++ src/components/AnswerItem.js
@@ -1,11 +1,3 @@
-const SpendTime = ({ answerItem }) => {
-  const answers = "";
-  for (const [key, value] of Object.entries(answerItem.spendTime)) {
-    if (value) answers += <p>{key}</p>;
-  }
-  return answers;
-};
-
 const AnswerItem = ({ answerItem }) => {
   return (
     <li>
@@ -15,10 +7,10 @@ const AnswerItem = ({ answerItem }) => {
           <em>How do you rate your rubber duck colour?</em>
           <span className="answer__line">{answerItem.review}</span>
         </p>
-        <p>
+        {/* <p>
           <em>How do you like to spend time with your rubber duck?</em>
-          <SpendTime answerItem={answerItem} />
-        </p>
+         
+        </p> */}
         <p>
           <em>What else have you got to say about your rubber duck?</em>
           <span className="answer__line">{answerItem.review}</span>
